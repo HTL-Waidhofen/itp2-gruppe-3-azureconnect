7da9f7ef1e078dae8442bb113e68545049d69ecf
Schmidbauer Florian <florian.schmidbauer@htlwy.at>
2024-05-29 08:27:38 +0200

    customize setup

---
+++ API_nodeJs/Project/ms-identity-node-main/App/public/scripts/customize.js
@@ -0,0 +1,46 @@
+function changeColor(id){
+    const root = document.querySelector(':root');
+
+    if(id == 1){
+        root.style.setProperty('--foreground-color', '#eee');
+        root.style.setProperty('--background-color', '#223');
+        root.style.setProperty('--highlight-color', '#493b81');
+    }
+    else if(id == 2){
+        root.style.setProperty('--foreground-color', '#efe');
+        root.style.setProperty('--background-color', '#232');
+        root.style.setProperty('--highlight-color', '#493b81');
+    }
+    else if(id == 3){
+        root.style.setProperty('--foreground-color', '#eee');
+        root.style.setProperty('--background-color', '#223');
+        root.style.setProperty('--highlight-color', '#493b81');
+    }
+    else if(id == 4){
+        root.style.setProperty('--foreground-color', '#eee');
+        root.style.setProperty('--background-color', '#223');
+        root.style.setProperty('--highlight-color', '#493b81');
+    }
+    else if(id == 5){
+        root.style.setProperty('--foreground-color', '#eee');
+        root.style.setProperty('--background-color', '#223');
+        root.style.setProperty('--highlight-color', '#493b81');
+    }
+}
+
+function changeFontsize(size){
+    const root = document.querySelector(':root');
+
+    if(size == 's'){
+        root.style.setProperty('--medium-text-size', '16px');
+        root.style.setProperty('--small-text-size', '12px');
+    }
+    else if(size == 'm'){
+        root.style.setProperty('--medium-text-size', '20px');
+        root.style.setProperty('--small-text-size', '16px');
+    }
+    else if(size == 'l'){
+        root.style.setProperty('--medium-text-size', '24px');
+        root.style.setProperty('--small-text-size', '20px');
+    }
+}
