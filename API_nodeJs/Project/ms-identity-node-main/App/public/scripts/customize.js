4c043a349a7dcd70e8cb4dd8185fe2068770cc59
KimmeswengerDavid <david.kimmeswenger@htlwy.at>
2024-06-06 09:03:52 +0200

    updated ChangeColor

--- API_nodeJs/Project/ms-identity-node-main/App/public/scripts/customize.js
+++ API_nodeJs/Project/ms-identity-node-main/App/public/scripts/customize.js
@@ -7,24 +7,24 @@ function changeColor(id){
         root.style.setProperty('--highlight-color', '#493b81');
     }
     else if(id == 2){
-        root.style.setProperty('--foreground-color', '#223');
-        root.style.setProperty('--background-color', '#eee');
-        root.style.setProperty('--highlight-color', '#493b81');
+        root.style.setProperty('--foreground-color', '#FCD752');
+        root.style.setProperty('--background-color', '#6184C6');
+        root.style.setProperty('--highlight-color', '#FCA2B9');
     }
     else if(id == 3){
-        root.style.setProperty('--foreground-color', '#eee');
-        root.style.setProperty('--background-color', '#223');
-        root.style.setProperty('--highlight-color', '#493b81');
+        root.style.setProperty('--foreground-color', '#F2EBE5');
+        root.style.setProperty('--background-color', '#9F496E');
+        root.style.setProperty('--highlight-color', '#9CAECE');
     }
     else if(id == 4){
-        root.style.setProperty('--foreground-color', '#eee');
-        root.style.setProperty('--background-color', '#223');
-        root.style.setProperty('--highlight-color', '#493b81');
+        root.style.setProperty('--foreground-color', '#F7F1ED');
+        root.style.setProperty('--background-color', '#3D5A5B');
+        root.style.setProperty('--highlight-color', '#CC8B65');
     }
     else if(id == 5){
-        root.style.setProperty('--foreground-color', '#eee');
-        root.style.setProperty('--background-color', '#223');
-        root.style.setProperty('--highlight-color', '#493b81');
+        root.style.setProperty('--foreground-color', '#F8EFEA');
+        root.style.setProperty('--background-color', '#E0475B');
+        root.style.setProperty('--highlight-color', '#333333');
     }
 }
 
